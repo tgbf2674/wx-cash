166fbc48668cc7a9a022f2f2199ed59022d764e4
maqing <136518041@qq.com>
2021-09-26 12:43:34 +0800

    show category name

--- src/components/EachRecord.tsx
+++ src/components/EachRecord.tsx
@@ -39,7 +39,7 @@ const EachRecord: React.FC<TProps> = (props) => {
     <StyledEachRecord to={`/record/${id}`}>
       <Category category={category!} recordType={type}/>
       <div className="record-content">
-        <div>其他</div>
+        <div>{category && category.name}</div>
         <div className="record-content-details">
           <span>{time}</span>
           <Divider gap={8}/>
